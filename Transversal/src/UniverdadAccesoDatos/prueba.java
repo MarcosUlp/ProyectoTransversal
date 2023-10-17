ab2cec023730510c76bc0c023013e9bf3d279245
ariansara <102766969+ariansara@users.noreply.github.com>
2023-10-17 16:51:20 -0300

    main prueba

---
+++ Transversal/src/UniverdadAccesoDatos/prueba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UniverdadAccesoDatos;
+
+/**
+ *
+ * @author pollo
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        AlumnoData a = new AlumnoData();
+        a.guardarAlumno("55325, 'prue', 'apellido', '1000-08-7', true");
+    }
+    
+}
