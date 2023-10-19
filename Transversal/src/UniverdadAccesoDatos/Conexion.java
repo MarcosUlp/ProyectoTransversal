c6f8b3555e29ce09585b47700de88ab3a9b6ea3e
ariansara <102766969+ariansara@users.noreply.github.com>
2023-10-19 18:58:31 -0300

    conexion

--- Transversal/src/UniverdadAccesoDatos/Conexion.java
+++ Transversal/src/UniverdadAccesoDatos/Conexion.java
@@ -7,10 +7,11 @@ package UniverdadAccesoDatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.JOptionPane;
 
 /**
@@ -18,43 +19,87 @@ import javax.swing.JOptionPane;
  * @author Emanuel Sosa
  */
 public class Conexion {
-
-    private static final String URL = "jdbc:mariadb://localhost:3306/";
-    private static final String DB = "univesidadg3";
-    private static final String usuario = "root";
-    private static final String password = "";
-
-    public static Connection connection;
-
-    private Conexion() {
-    }
+     private static final String DATABASE_URL = "jdbc:mariadb://";
+    private static final String DATABASE_HOST = "localhost";
+    private static final String DATABASE_PUERTO = ":3306";
+    private static final String DATABASE_DB = "/univesidadg3";
+    private static final String DATABASE_USUARIO = "root";
+    private static final String DATABASE_PASSWORD = "";
+    private static Conexion conexion = null;
     
-    public static Connection getConexion(){
-    if(connection==null){
-            try {
+    private Conexion(String jdbcmysqllocalhostuniversidad, String root, String string) {
+        try {
+            //Cargar drivers
             Class.forName("org.mariadb.jdbc.Driver");
-            Connection con = DriverManager.getConnection(URL+DB, usuario, password);
-            JOptionPane.showMessageDialog(null, "conectado");
             
-        } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "Error al cargar Driver");
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error de conexión");
-
-            int error = ex.getErrorCode();
-            ex.printStackTrace();
-            
-            if (error == 1146) {
-                JOptionPane.showMessageDialog(null, "Tabla inexistente");
-            } else if (error == 1062) {
-                JOptionPane.showMessageDialog(null, "Dni duplicado");
-            } else if (error == 1049) {
-                JOptionPane.showMessageDialog(null, "BD inexistente");
-            } else {
-                JOptionPane.showMessageDialog(null, "Error SQL");
-            }
+        } catch ( ClassNotFoundException ex ) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver" + ex);
         }
     }
-             return connection;
+
+    public static Connection getConexion() {
+        Connection con = null;
+        
+        if ( conexion == null ) {
+            conexion = new Conexion("jdbc:mysql://localhost/universidad", "root", "");
+        }
+        
+        try {
+            
+            con = DriverManager.getConnection(
+                    DATABASE_URL + 
+                    DATABASE_HOST + 
+                    DATABASE_PUERTO + 
+                    DATABASE_DB + 
+                    "?useLegacyDatetimeCode=false&serverTimezone=UTC" + 
+                    "&user=" + 
+                    DATABASE_USUARIO + 
+                    "&password=" + 
+                    DATABASE_PASSWORD
+            );
+            
+        }catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex.getMessage() );
+        }
+        
+        return con;
     }
 }
+
+
+//    private static final String URL = "jdbc:mariadb://localhost:3306/univesidadg3";
+//    private static final String usuario = "root";
+//    private static final String password = "";
+//
+//    private static Connection connection;
+//
+//    Conexion() {
+//    }
+//
+//    public static Connection getConexion() {
+//        if (connection == null) {
+//            try {
+//                Class.forName("org.mariadb.jdbc.Driver");
+//                Connection con = DriverManager.getConnection(URL, usuario, password);
+//
+//            } catch (ClassNotFoundException ex) {
+//                JOptionPane.showMessageDialog(null, "Error al cargar Driver");
+//            } catch (SQLException ex) {
+//                JOptionPane.showMessageDialog(null, "Error de conexión");
+//
+//                int error = ex.getErrorCode();
+//                ex.printStackTrace();
+//
+//                if (error == 1146) {
+//                    JOptionPane.showMessageDialog(null, "Tabla inexistente");
+//                } else if (error == 1062) {
+//                    JOptionPane.showMessageDialog(null, "Dni duplicado");
+//                } else if (error == 1049) {
+//                    JOptionPane.showMessageDialog(null, "BD inexistente");
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "Error SQL");
+//                }
+//            }
+//            return connection
+//        }
+
